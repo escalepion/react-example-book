f49542eb5f341c3614523d0f067f0e413f6a1241
Akif MUCURLUOĞLU <akif.mucurluoglu@akinon.com>
2018-05-25 16:10:26 +0300

    change

--- src/components/indexPage/IndexPage.js
+++ src/components/indexPage/IndexPage.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-import logo from '../../logo.svg';
-import '../../App.css';
+import logo from "../../logo.svg";
+import "../../App.css";
 
 class IndexPage extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
-          You can see all examples that i faced while i was developing react applications. It s not just about
-          react, this guide also contains es6, redux, redux-form, other react 3. party components etc. This is my 
+      <div className="container">
+        <p>
+          You can see all examples that i faced while i was developing react
+          applications. It s not just about react, this guide also contains es6,
+          redux, redux-form, other react 3. party components etc. This is my
           opensource notes.
         </p>
-        <p><Link to='/state_change'>State change</Link></p>
+        <p>
+          <Link to="/state_change">State change</Link>
+        </p>
       </div>
     );
   }
