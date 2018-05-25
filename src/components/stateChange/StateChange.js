cd667b0290f410448437ff27f2e9150e51e806a9
Akif MUCURLUOĞLU <akif.mucurluoglu@akinon.com>
2018-05-25 17:25:35 +0300

    add state change component

--- src/components/stateChange/StateChange.js
+++ src/components/stateChange/StateChange.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
 class StateChange extends Component {
+    constructor() {
+      super();
+      this.state = {
+        backgroundLight : true 
+      }
+    }
+    handleClick() {
+      this.setState({ backgroundLight : !this.state.backgroundLight });
+    }
     render() {
+      const { divLightStyle, divDarkStyle, containerDiv } = styles;
         return (
-            <div>
-                State Change Component
+            <div className="row">
+              <div className="col-sm-4">
+                <button 
+                onClick={this.handleClick.bind(this)}
+                className="btn btn-block btn-primary"
+                >Toggle Color</button>
+              </div>
+              <div className="col-sm-8">
+                <div style={{...containerDiv, backgroundColor: this.state.backgroundLight ? 'yellow' : '#222' }}></div>
+              </div>
             </div>
         );
     }
+}
+
+const styles = {
+  containerDiv: {
+    width: '100%',
+    height: '30rem'
+  }
 }
 
 export default StateChange;
